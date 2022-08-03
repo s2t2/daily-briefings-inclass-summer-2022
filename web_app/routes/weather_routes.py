1c884c95c9f2d8b2b4bc1765d4982e5461e3cf3a
MJ Rossetti <s2t2@users.noreply.github.com>
2022-08-03 10:11:40 -0400

    API route

--- web_app/routes/weather_routes.py
+++ web_app/routes/weather_routes.py
@@ -30,3 +30,22 @@ def weather_forecast():
     else:
         #flash("Geography Error. Please try again!", "danger")
         return redirect("/weather/form")
+
+
+
+
+@weather_routes.route("/api/weather/forecast.json")
+def weather_forecast_api():
+    print("WEATHER FORECAST (API)...")
+
+    url_params = dict(request.args)
+    print("URL PARAMS:", url_params)
+
+    country_code = url_params.get("country_code") or "US"
+    zip_code = url_params.get("zip_code") or "20057"
+
+    results = get_hourly_forecasts(country_code=country_code, zip_code=zip_code)
+    if results:
+        return jsonify(results)
+    else:
+        return jsonify({"message":"Invalid Geography. Please try again."}), 404
